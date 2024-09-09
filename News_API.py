d6344aeb571ef930b446096684c2330c595a6583
MinchiPt <85115220+MinchiPt@users.noreply.github.com>
2024-09-09 08:51:55 +0800

    Update News_API.py

--- News_API.py
+++ News_API.py
@@ -1,6 +1,6 @@
 import requests
 
-r = requests.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=3cdac0182a7f40afb1c4d10bfacf9230')
+r = requests.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=<api-key>')
 content = r.json()
 print(content['articles']) 
 #[print(content['articles'][0]['title'])
